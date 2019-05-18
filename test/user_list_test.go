1227a6ccd8565ef1e34274360b62ba827c6f7973
hawklithm <woshipmx1991@sina.com>
2019-05-18 16:12:18 +0800

    project init

---
+++ test/user_list_test.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"flag"
+	"github.com/hawklithm/anychatcmd/ui"
+	"github.com/hawklithm/termui"
+	"log"
+	"os"
+	"testing"
+	"time"
+)
+
+func Test_UserList(t *testing.T) {
+	flag.Parse()
+
+	userInfoArray := []ui.UserInfo{
+		{UserId: "12312124", Nick: "TestNick1",
+			LastChatTime: time.Now()},
+		{UserId: "12312125", Nick: "TestNick2",
+			DisplayName:  "TestDisplay2",
+			LastChatTime: time.Now()},
+	}
+
+	ruserInfoArray := []ui.UserInfo{
+		{UserId: "12312128", Nick: "TestNick3",
+			LastChatTime: time.Now()},
+		{UserId: "12312129", Nick: "TestNick4",
+			DisplayName:  "TestDisplay4",
+			LastChatTime: time.Now()},
+	}
+
+	groupArray := []ui.Group{
+		{GroupId: "12312126", Name: "TestGroup1",
+			LastChatTime: time.Now()},
+		{GroupId: "12312127", Name: "TestGroup2",
+			LastChatTime: time.Now()},
+	}
+
+	rgroupArray := []ui.Group{
+		{GroupId: "12312130", Name: "TestGroup3",
+			LastChatTime: time.Now()},
+		{GroupId: "12312131", Name: "TestGroup4",
+			LastChatTime: time.Now()},
+	}
+
+	fileName := "log.txt"
+	var logFile *os.File
+	logFile, err := os.OpenFile(fileName, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
+	defer logFile.Close()
+
+	err = termui.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer termui.Close()
+
+	if err != nil {
+		print("打开文件失败!\n")
+	}
+
+	logger := log.New(logFile, "[*]", log.LstdFlags)
+
+	userList := ui.NewUserList(ruserInfoArray, rgroupArray, userInfoArray,
+		groupArray,
+		nil, 40, 40, 0, 0, logger)
+
+	userList.Pick()
+
+	uiEvents := termui.PollEvents()
+
+	for {
+		e := <-uiEvents
+		if userList.Action(e) {
+			continue
+		}
+		switch e.ID {
+		case "<C-c>":
+			return
+		}
+	}
+
+}
---
+++ ui/user_list.go
@@ -0,0 +1,236 @@
+package ui
+
+import (
+	"github.com/hawklithm/anychatcmd/utils"
+	ui "github.com/hawklithm/termui"
+	"github.com/hawklithm/termui/widgets"
+	"log"
+	"time"
+)
+
+type UserList struct {
+	currentId       string
+	curUserIndex    int
+	logger          *log.Logger
+	baseX           int
+	baseY           int
+	width           int
+	height          int
+	recentUserList  []UserInfo
+	recentGroupList []Group
+	userList        []UserInfo
+	groupList       []Group
+	userChangeEvent chan UserChangeEvent
+
+	tabPane          *widgets.TabPane
+	userNickListBox  *widgets.List
+	groupNickListBox *widgets.List
+	recentListBox    *widgets.List
+	tabWidgets       []*widgets.List
+	picked           bool
+	currentTab       *widgets.List
+}
+
+func (this *UserList) Pick() {
+	this.picked = true
+}
+
+func (this *UserList) Unpick() {
+	this.picked = false
+}
+
+type UserChangeEvent struct {
+}
+
+type UserInfo struct {
+	UserId       string
+	Nick         string
+	DisplayName  string
+	LastChatTime time.Time
+}
+
+//用户群组
+type Group struct {
+	UserList     []UserInfo
+	Name         string
+	GroupId      string
+	LastChatTime time.Time
+}
+
+type SortItem struct {
+	Nick         string
+	Id           string
+	LastChatTime time.Time
+}
+
+type SortItems []SortItem
+
+func (p SortItems) Len() int { return len(p) }
+
+// 根据元素的年龄降序排序 （此处按照自己的业务逻辑写）
+func (p SortItems) Less(i, j int) bool {
+	return p[i].LastChatTime.After(p[j].LastChatTime)
+}
+
+func merge(userInfo []UserInfo, groupInfo []Group) *SortItems {
+	z := make(SortItems, len(userInfo)+len(groupInfo))
+	for i, user := range userInfo {
+		z[i] = SortItem{
+			Nick:         utils.If(user.DisplayName != "", user.DisplayName, user.Nick).(string),
+			Id:           user.UserId,
+			LastChatTime: user.LastChatTime,
+		}
+	}
+	return &z
+}
+
+func (this *UserList) renderTab() {
+	this.currentTab = this.tabWidgets[this.tabPane.ActiveTabIndex]
+	ui.Render(this.tabPane, this.currentTab)
+}
+
+func (this *UserList) Reset() {
+	this.userNickListBox = widgets.NewList()
+	//userNickListBox.Title = "用户列表"
+	//userNickListBox.BorderStyle = ui.NewStyle(ui.ColorMagenta)
+	//userNickListBox.Border = true
+	this.userNickListBox.TextStyle = ui.NewStyle(ui.ColorYellow)
+	this.userNickListBox.WrapText = false
+	this.userNickListBox.SelectedRowStyle = ui.NewStyle(ui.ColorWhite,
+		ui.ColorRed)
+
+	this.userNickListBox.SetRect(this.baseX, this.baseY+2,
+		this.baseX+this.width,
+		this.baseY+4+this.height)
+
+	groupNickListBox := widgets.NewList()
+	//userNickListBox.Title = "用户列表"
+	//userNickListBox.BorderStyle = ui.NewStyle(ui.ColorMagenta)
+	//userNickListBox.Border = true
+	groupNickListBox.TextStyle = ui.NewStyle(ui.ColorYellow)
+	groupNickListBox.WrapText = false
+	groupNickListBox.SelectedRowStyle = ui.NewStyle(ui.ColorWhite, ui.ColorRed)
+
+	groupNickListBox.SetRect(this.baseX, this.baseY+2, this.baseX+this.width,
+		this.baseY+4+this.height)
+
+	this.userNickListBox = widgets.NewList()
+	//userNickListBox.Title = "用户列表"
+	//userNickListBox.BorderStyle = ui.NewStyle(ui.ColorMagenta)
+	//userNickListBox.Border = true
+	this.userNickListBox.TextStyle = ui.NewStyle(ui.ColorYellow)
+	this.userNickListBox.WrapText = false
+	this.userNickListBox.SelectedRowStyle = ui.NewStyle(ui.ColorWhite,
+		ui.ColorRed)
+
+	this.userNickListBox.SetRect(this.baseX, this.baseY+2,
+		this.baseX+this.width,
+		this.baseY+4+this.height)
+
+	this.groupNickListBox = groupNickListBox
+
+	nickList := make([]string, len(this.userList))
+	groupList := make([]string, len(this.groupList))
+
+	for i, user := range this.userList {
+		if user.DisplayName != "" {
+			nickList[i] = user.DisplayName
+		} else {
+			nickList[i] = user.Nick
+		}
+	}
+	this.userNickListBox.Rows = nickList
+	this.logger.Println("nickList=", nickList)
+
+	for i, group := range this.groupList {
+		groupList[i] = group.Name
+	}
+	this.groupNickListBox.Rows = groupList
+	this.logger.Println("groupList=", groupList)
+
+	//recentList := merge(this.recentUserList, this.recentGroupList)
+
+	this.tabPane = widgets.NewTabPane("好友列表", "群列表")
+	this.tabPane.SetRect(this.baseX, this.baseY, this.baseX+this.width,
+		this.baseY+3)
+	this.tabPane.Border = true
+	tabWidgets := make([]*widgets.List, 2)
+	tabWidgets[0] = this.userNickListBox
+	tabWidgets[1] = this.groupNickListBox
+	this.tabWidgets = tabWidgets
+
+	this.currentTab = this.userNickListBox
+
+	ui.Render(this.tabPane, this.currentTab)
+}
+
+func NewUserList(recentUserList []UserInfo, recentGroupList []Group, userList []UserInfo,
+	groupList []Group,
+	userChangeEvent chan UserChangeEvent, width, height, baseX, baseY int,
+	logger *log.Logger) *UserList {
+
+	//	chinese := false
+
+	l := &UserList{
+		curUserIndex:    0,
+		logger:          logger,
+		baseX:           baseX,
+		baseY:           baseY,
+		width:           width,
+		height:          height,
+		recentUserList:  recentUserList,
+		recentGroupList: recentGroupList,
+		userList:        userList,
+		groupList:       groupList,
+		userChangeEvent: userChangeEvent,
+	}
+
+	l.Reset()
+
+	return l
+
+}
+
+func (l *UserList) nextUser() {
+	l.currentTab.ScrollDown()
+	l.curUserIndex = l.currentTab.SelectedRow
+	ui.Render(l.currentTab)
+}
+
+func (l *UserList) prevUser() {
+	l.currentTab.ScrollUp()
+	l.curUserIndex = l.currentTab.SelectedRow
+	ui.Render(l.currentTab)
+}
+
+func (l *UserList) focuseLeft() {
+	l.tabPane.FocusLeft()
+	l.renderTab()
+}
+
+func (l *UserList) focuseRight() {
+	l.tabPane.FocusRight()
+	l.renderTab()
+}
+
+func (this *UserList) Action(e ui.Event) bool {
+	if !this.picked {
+		return false
+	}
+	switch e.ID {
+	case "j":
+		this.nextUser()
+		return true
+	case "k":
+		this.prevUser()
+		return true
+	case "h":
+		this.focuseLeft()
+		return true
+	case "l":
+		this.focuseRight()
+		return true
+	}
+	return false
+
+}
